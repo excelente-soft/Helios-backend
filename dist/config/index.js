615006c0a559afc7f59a4ee676e441a0b12e4b1e
Andrew <MoonHeiz@yandex.by>
2022-03-17 20:20:25 +0300

    refactor(setup): remove build folder

--- dist/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var _a, _b, _c;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VERSION = exports.PORT = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)({ path: '.env' });
-_a = process.env, _b = _a.PORT, exports.PORT = _b === void 0 ? 5000 : _b, _c = _a.VERSION, exports.VERSION = _c === void 0 ? 1 : _c;
--- dist/controllers/auth.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginController = void 0;
-const loginController = async (req, res) => {
-    res.status(200).json({ name: 'Andrew' });
-};
-exports.loginController = loginController;
